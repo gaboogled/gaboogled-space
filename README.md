626ff6cfe855a746846f2c944c5fbb63ea55cc29
gaboogled <gaboogled@gmail.com>
2019-04-10 20:14:49 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# gaboogled-space
---
+++ _b/h5bp-index.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html class="no-js" lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <title>Gaboogled Developemt</title>
+  <meta name="description" content="">
+  <link rel="manifest" href="site.webmanifest">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="apple-touch-icon" href="resources/images/icon.png">
+  <link rel="stylesheet" href="resources/css/gaboogled.css">
+  <meta name="theme-color" content="#ffffff">
+</head>
+
+<body>
+
+  <!--[if IE]>
+    <div class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</div>
+  <![endif]-->
+
+        <section>
+         <!-- Add your site or application between here -->
+            <p>Hello world! This is HTML5 Boilerplate.</p>
+         <!-- Add your site or application between here -->
+       </section>
+
+  <!----------------------------------------------  Footer  ------------------------------------------------------>
+  HTML 5 Boilerplate JavaScript files.
+  <script src="resources/js/vendor/modernizr-3.7.1.min.js"></script>
+  <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+  <script>window.jQuery || document.write('<script src="resources/js/vendor/jquery-3.3.1.min.js"><\/script>')</script>
+  <script src="resources/js/h5bp-plugins.js"></script>
+  <script src="resources/js/h5bp.js"></script>
+
+  <!------------------------ Google Analytics: change UA-XXXXX-Y to be your site's ID. --------------------------->
+  <script>
+    window.ga = function () { ga.q.push(arguments) }; ga.q = []; ga.l = +new Date;
+    ga('create', 'UA-XXXXX-Y', 'auto'); ga('set','transport','beacon'); ga('send', 'pageview')
+  </script>
+  <script src="https://www.google-analytics.com/analytics.js" async defer></script>
+
+</body>
+</html>
